ac07be8a2f91fde595a76ad4fde95a6761c125f0
Pauline Gobé <pauline.gobe16@gmail.com>
2023-04-19 21:50:40 +0200

    clean boid class

--- src/Boid.hpp
+++ src/Boid.hpp
@@ -5,48 +5,44 @@
 
 #pragma once
 
+struct Params {
+    float cohesion_magnitude{10};
+    float alignment_magnitude;
+    float separation_magnitude;
+    float distance_max;
+    float speed_factor;
+};
+
 class Boid {
 private:
     // attributes
-    glm::vec3 m_pos;       // position
-    glm::vec3 m_vel;       // velocity
-    glm::vec3 m_direction; // direction
+    glm::vec3 m_pos;                                                                         // position
+    glm::vec3 get_vel(float speed_factor) { return glm::vec3(speed_factor) * m_direction; }; // velocity // TODO replace vel variable with a getter that computes it on the fly
+    glm::vec3 m_direction;                                                                   // direction
 
 public:
-    static float cohesion_magnitude;
-    static float alignment_magnitude;
-    static float separation_magnitude;
-    static float distance_max;
-    static float speed_factor;
-
     /* CONSTRUCTORS */
     Boid()
-        : m_pos(0), m_vel(0), m_direction(p6::random::direction(), 0){};
+        : m_direction(p6::random::direction(), 0)
+    {
+        m_pos = glm::vec3(p6::random::number(-2, 2), p6::random::number(-1, 1), p6::random::number(-2, 0));
+    };
     Boid(glm::vec3 position)
-        : m_pos(position), m_vel(0), m_direction(p6::random::direction(), 0){};
-
-    /* DESTRUCTOR */
-    ~Boid() = default;
+        : m_pos(position), m_direction(p6::random::direction(), 0){};
 
     /* GETTEUR */
     glm::vec3 get_pos() const { return m_pos; }
-    glm::vec3 get_vel() const { return m_vel; }
+    // glm::vec3 get_vel() const { return m_vel; }
     glm::vec3 get_direction() const { return m_direction; }
 
     /* SETTEUR */
     void set_pos(const glm::vec3 position) { m_pos = position; }
 
-    /* DISPLAY */
-    void display();
-
     /* BOID VELOCITY */
 
-    void update_position(const float delta_time, const float ratio);
-    void update_velocity();
-    void update_direction(const std::vector<Boid>& boids);
-    // void update_acceleration();
-
-    /* CALCULS */
+    void update(const float delta_time, const float ratio, const float speed_factor, const std::vector<Boid>& boids, Params& params);
+    void update_position(const float delta_time, const float ratio, const float speed_factor); // TODO make an update function that calls all the nceessary updates
+    void update_direction(const std::vector<Boid>& boids, const Params& params);
 
     /* NEIGHBORS */
 
@@ -55,14 +51,17 @@ public:
     /* BOID RULES */
 
     // Cohesion
-    glm::vec3 cohesion(const std::vector<Boid>& boids);
+    glm::vec3 cohesion(const std::vector<Boid>& neighbors);
 
     // Separation
-    glm::vec3 separation(const std::vector<Boid>& boids);
+    glm::vec3 separation(const std::vector<Boid>& neighbors);
 
     // Alignment
-    glm::vec3 alignment(const std::vector<Boid>& boids);
-
-    /* BOID WITH WINDOW */
-    float stay_in_world(const float& value, const float& max, const float& min);
+    glm::vec3 alignment(const std::vector<Boid>& neighbors);
 };
+
+/* WORLD */
+float stay_in_world(const float& value, const float& max, const float& min);
+
+/* DIPLAY */
+void display(); // TODO fonction libre
