9e3c9c1f99c6403ca3c321304f0ac6a0a7ad3e76
aur8 <aurelien.drt@gmail.com>
2023-05-10 14:10:03 +0200

    fix issue

--- src/main.cpp
+++ src/main.cpp
@@ -89,6 +89,15 @@ int main() {
   // binding vbo
   glBindBuffer(GL_ARRAY_BUFFER, vbo_boids);
 
+  // VBO CUBE
+  GLuint vbo_cube = 0;
+  glGenBuffers(1, &vbo_cube);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_boids);
+
+  // création CUBE
+
+  /// TODO vertices cube
+
   const GLuint VERTEX_ATTR_POSITION = 0;
   const GLuint VERTEX_ATTR_NORMAL = 1;
   const GLuint VERTEX_ATTR_TEXCOORDS = 2;
